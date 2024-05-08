ea527b5ee8ed43688123dd519093d94d235dfc4e
Prashant Salve <128687203+prashant04071995@users.noreply.github.com>
2024-05-08 13:12:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-//your JS code here. If required.
-        // Listen for the DOMContentLoaded event which fires when the DOM is fully loaded
-        document.addEventListener('DOMContentLoaded', function() {
-            // Check if the body has no child nodes
-            if (document.body.children.length === 0) {
-                // Create a text node with the message
-                var textNode = document.createTextNode("DOM load success");
-                // Append the text node to the body
-                document.body.appendChild(textNode);
-            }
-        });
-    
+ document.addEventListener("DOMContentLoaded", function () {
+        const body = document.querySelector("body");
+        const text = document.createTextNode("DOM load success");
+        body.appendChild(text);
+      });
